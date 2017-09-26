4d2c2eb45f6d3bebf7fb99b924c8413cd2a561b3
Muraliduke <muraliduke4461@gmail.com>
2017-09-26 09:53:19 +0530

    Create service.ts

---
+++ service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject }    from 'rxjs/Subject';
+
+@Injectable()
+
+export class MissionService {
+
+   // Observable string sources
+  private caseNumber = new Subject<string>();  
+
+  // Observable string streams
+  caseNumber$ = this.caseNumber.asObservable();
+
+    // Service message commands
+  publishData(data: string) {
+    this.caseNumber.next(data);
+  }
+  
+}
